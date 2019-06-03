774a742161e4d495facfbe056c0bf9caa1635775
Zane Kuehn <zanekuehn@yahoo.com>
2019-06-03 13:55:08 -0500

    Added a variable to the function to declare year of birth

--- zdrills.js
+++ zdrills.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const greeting1=function createGreeting(name, age){
-return `Hi, my name is ${name} and im ${age} years old`;}
+const yearOfBirth= 2019-age;
+    return `Hi, my name is ${name} and im ${age} years old, I was born in ${yearOfBirth}`;};
 
 
 console.log(greeting1);
