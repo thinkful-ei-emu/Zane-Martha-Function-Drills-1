46432991f6741bc8be5a7ca1c2611c9a4ab8efd2
Zane Kuehn <zanekuehn@yahoo.com>
2019-06-03 15:22:52 -0500

    Finished Function Drills 1

--- zdrills.js
+++ zdrills.js
@@ -1,8 +1,42 @@
 "use strict";
 
-const greeting1=function createGreeting(name, age){
-const yearOfBirth= 2019-age;
-    return `Hi, my name is ${name} and im ${age} years old, I was born in ${yearOfBirth}`;};
+function getYearOfBirth(age){
+    if(age<0){
+        throw new Error("Age can not be negative.")
+    }
+    
+    return  2019-age
+    
+}
+
+function createGreeting(name, age){
+    if(age<0){
+        throw new Error('Age can not be negative.')
+    }
+    if( name === undefined|| age === undefined) {
+        throw new Error('Arguements not Valid')
+    } 
+    if(typeof name !=="string"){
+        throw new TypeError('Arguement is not a string');
+    }
+    if(typeof age !== 'number'){
+        throw new TypeError('Argument is not a number')
+    }
+    const yob=getYearOfBirth(age);
+    return `Hi, my name is ${name} and im ${age} years old, I was born in ${yob}`;
+}
+    
 
 
-console.log(greeting1);
+
+try{
+    const greeting1=createGreeting("Zane",27);
+    console.log(greeting1);
+} catch(e){
+    console.error(e.message)
+}
+
+
+    
+
+
