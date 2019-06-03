ec6ace9169a745536e2da2beb0306a8502d63735
Zane Kuehn <zanekuehn@yahoo.com>
2019-06-03 13:36:10 -0500

    Changed the console log within function to return the string and console logged the variable with function inside.

--- zdrills.js
+++ zdrills.js
@@ -1,4 +1,7 @@
 "use strict";
 
-function createGreeting(){
-console.log('Hi, my name is Zane and im 26 years old');}
+const greeting1=function createGreeting(){
+return 'Hi, my name is Zane and im 26 years old';}
+
+
+console.log(greeting1);
